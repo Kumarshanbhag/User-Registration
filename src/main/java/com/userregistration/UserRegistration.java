2fb26bd34b8e0992237b2f4ef78f4a513549fd9e
kumarshanbhag <kumarshanbhag302@gmail.com>
2020-02-17 19:23:38 +0530

    ADDED : Valid First Name Check

--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -1,7 +1,11 @@
 package com.userregistration;
-
 public class UserRegistration {
+    private final String VALID_NAME="^[A-Z][a-z]{2,}$";
     public static void main(String[] args) {
         System.out.println("Welcome To User Registration");
+    }
+
+    public boolean validateFirstName(String firstName) {
+        return firstName.matches(VALID_NAME);
     }
 }
